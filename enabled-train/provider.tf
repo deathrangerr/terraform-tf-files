71dee5be7a0076a7572a190230419630caa9221f
Death Ranger <53964374+deathrangerr@users.noreply.github.com>
2021-11-15 21:00:11 +0530

    Update provider.tf

--- enabled-train/provider.tf
+++ enabled-train/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region     = "ap-south-1"
-    access_key = "AKIATZ6PZPLQSOUBTM26"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
